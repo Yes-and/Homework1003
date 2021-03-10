27f4eeaa6320485c660aef0d97ccd55a1f5679f4
Yes-and <72066894+Yes-and@users.noreply.github.com>
2021-03-10 13:38:13 +0100

    Updated the files

--- CleanString.java
+++ CleanString.java
@@ -6,6 +6,8 @@ public class CleanString {
         String[] characters = str.split("");
         int i = 0;
         while(i < characters.length-1) {
+
+            // Add a character to the string if the next one is different
             if(characters[i].equals(characters[i+1])) {
                 i -= -1;
             } else {
@@ -13,9 +15,13 @@ public class CleanString {
                 i += 1;
             }
         }
+        
+        // Check for the last character
         if(characters[i].equals(characters[i-1]) == false) {
             temp += characters[i];
         }
+
+        // Return the string
         return temp;
     }
     public static void main(String[] args) {
